689f484fa4553d923f10658318c271ea4f1c5c9d
Glitchbott <36093622+Glitchbott@users.noreply.github.com>
2018-03-15 16:21:21 +0600

    Comparison of interface types and abstract base classes

---
+++ ICloneableExample/ICloneableExample/CloneableType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableExample
+{
+  public abstract class CloneableType
+    {
+        //только производные типы могут поддерживать этот интерфейс.
+        public abstract object Clone();
+        public interface ICloneable
+        {
+            object Clone();
+        }
+       
+    }
+}
---
+++ ICloneableExample/ICloneableExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ICloneableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** A First at Interface ***\n");
+            //Все эти классы поддерживают интерфейс Cloneable
+            string myStr = "Hello";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn = new System.Data.SqlClient.SqlConnection();
+            // они могут быть переданы методу,принимающему ICloneable
+            CloneMe(myStr);
+            CloneMe(unixOS);
+            CloneMe(sqlCnn);
+
+            Console.ReadLine();
+
+             void CloneMe(ICloneable c)
+            {
+                //клонировать то что получено и вывести
+                object theClone = c.Clone();
+                Console.WriteLine("Your clone is a: {0}", theClone.GetType().Name);
+            }
+
+        }
+    }
+}
